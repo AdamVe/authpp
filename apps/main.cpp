fa6329940ae10eace7edfce20b35c7b05e90011c
Adam Velebil <pardalek@gmail.com>
2023-02-26 10:59:06 +0100

    main cleanup

--- apps/main.cpp
+++ apps/main.cpp
@@ -19,21 +19,15 @@ int main()
     Logger::setLevel(Logger::Level::kDebug);
 
     libusb_init(nullptr);
-    // libusb_set_option(nullptr, LIBUSB_OPTION_LOG_LEVEL, LIBUSB_LOG_LEVEL_DEBUG);
 
     auto* version { libusb_get_version() };
 
     log.i("Using libusb version: {}.{}.{}", version->major, version->minor,
         version->micro);
 
-    log.d("hot-plug support: {}", libusb_has_capability(LIBUSB_CAP_HAS_HOTPLUG));
-
-    // enumerate current USB devices
     libusb_device** devices;
 
     std::size_t connected_devices_count = libusb_get_device_list(NULL, &devices);
-
-    std::vector<libusb_device*> yubiKeyUsbDevices;
 
     for (std::size_t&& index = 0; index < connected_devices_count; ++index) {
         auto* d = devices[index];
@@ -41,15 +35,7 @@ int main()
         libusb_device_descriptor desc;
         libusb_get_device_descriptor(d, &desc);
         if (desc.idVendor == VENDOR_YUBICO) {
-            yubiKeyUsbDevices.push_back(d);
-        }
-    }
-
-    if (yubiKeyUsbDevices.size() == 0) {
-        log.e("No connected YubiKey detected. Please connect a test YubiKey");
-    } else {
-        for (auto&& yubiKeyUsbDevice : yubiKeyUsbDevices) {
-            UsbDevice yubiKey(yubiKeyUsbDevice);
+            UsbDevice yubiKey(d);
             log.d("Yubikey: {}", yubiKey.ToString());
 
             UsbDevice::Connection usbConnection(yubiKey);
