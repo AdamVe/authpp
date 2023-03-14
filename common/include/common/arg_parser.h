610bcea89dfa0ed2efd34caeb40820dd583007b5
Adam Velebil <pardalek@gmail.com>
2023-03-14 23:04:09 +0100

    update arg_parser

--- common/include/common/arg_parser.h
+++ common/include/common/arg_parser.h
@@ -1,4 +1,3 @@
-#include <algorithm>
 #include <span>
 #include <string>
 #include <vector>
@@ -6,43 +5,13 @@
 namespace authpp::common {
 class ArgParser {
 public:
-    ArgParser(int argc, char** argv)
-        : args(buildArgs(argc, argv))
-    {
-    }
-
-    bool hasParam(std::string_view value) const
-    {
-        // return std::ranges::count(args, value) > 0;
-        return std::find(args.cbegin(), args.cend(), std::string(value)) != args.cend();
-    }
-
-    std::string getParamValue(std::string_view value) const
-    {
-#if __cpp_ranges_lib > 22010101L
-        auto i = std::ranges::find(params, value);
-#else
-        auto i = std::find(args.cbegin(), args.cend(), value);
-#endif
-        if (i++ != args.cend() && i != args.cend()) {
-            return *i;
-        }
-        return {};
-    }
+    ArgParser(int argc, char** argv);
+    bool hasParam(std::string_view value) const;
+    std::string getParamValue(std::string_view value) const;
 
 private:
     using args_t = std::vector<std::string>;
-
-    args_t buildArgs(int argc, char** argv)
-    {
-        args_t retval;
-        std::span args_span { argv, argv + argc };
-        for (auto&& arg : args_span) {
-            retval.emplace_back(arg);
-        }
-        return retval;
-    }
-
+    args_t buildArgs(int argc, char** argv) const;
     const args_t args;
 };
-}
+} // namespace authpp::common
--- common/src/common/arg_parser.cpp
+++ common/src/common/arg_parser.cpp
@@ -1,1 +1,42 @@
 #include "arg_parser.h"
+
+#include <algorithm>
+
+namespace authpp::common {
+ArgParser::ArgParser(int argc, char** argv)
+    : args(buildArgs(argc, argv))
+{
+}
+
+bool ArgParser::hasParam(std::string_view value) const
+{
+#if __cpp_ranges_lib > 22010101L
+    return std::ranges::count(args, value) > 0;
+#else
+    return std::find(args.cbegin(), args.cend(), std::string(value)) != args.cend();
+#endif
+}
+
+std::string ArgParser::getParamValue(std::string_view value) const
+{
+#if __cpp_ranges_lib > 22010101L
+    auto i = std::ranges::find(params, value);
+#else
+    auto i = std::find(args.cbegin(), args.cend(), value);
+#endif
+    if (i++ != args.cend() && i != args.cend()) {
+        return *i;
+    }
+    return {};
+}
+
+ArgParser::args_t ArgParser::buildArgs(int argc, char** argv) const
+{
+    args_t retval;
+    std::span args_span { argv, argv + argc };
+    for (auto&& arg : args_span) {
+        retval.emplace_back(arg);
+    }
+    return retval;
+}
+} // namespace authpp::common
